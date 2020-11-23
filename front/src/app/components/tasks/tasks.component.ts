85e526208903d0d9dcfb3d222dd61654cde7e4e5
oleg <olegd1c@gmail.com>
2020-11-23 09:44:18 +0200

    add permission update story points

--- front/src/app/components/tasks/tasks.component.ts
+++ front/src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TaskService } from '@services/task.service';
-import {Task} from "@shared_models/task.model";
+import { Task } from "@shared_models/task.model";
+import { Permissions } from "@shared_models/permission.enum";
 
 @Component({
   selector: 'app-tasks',
@@ -17,6 +18,7 @@ export class TasksComponent implements OnInit {
   reloadTasks = false;
   paramsSearch;
   loadingStoryPoints = false;
+  permissions = Permissions;
 
   constructor(private taskService: TaskService) { }
 
